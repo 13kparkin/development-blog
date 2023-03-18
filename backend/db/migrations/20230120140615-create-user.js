b893ff9e3470da87ac0f483ed8641a354072cfd7
Kyle Parkin <13ktparkin@gmail.com>
2023-03-18 19:05:33 +0000

    testing

--- backend/db/migrations/20230120140615-create-user.js
+++ backend/db/migrations/20230120140615-create-user.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.createTable("Users", {
